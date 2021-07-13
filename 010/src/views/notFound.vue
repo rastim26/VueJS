e9a2e2d6e1974382bbbb4fd58b4ce4985c7415c7
RASTIM SAFAROV <52628566+rastim26@users.noreply.github.com>
2021-07-13 20:30:39 +0300

    10 lesson

---
+++ 010/src/views/notFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="card">
+    <h1>Страница не найдена!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, impedit.</p>
+    <router-link to="login">На главную</router-link>
+  </div>
+</template>
