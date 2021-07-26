34c2b3297b02b221fb2caf0828a9e2200c21d8d9
RASTIM SAFAROV <52628566+rastim26@users.noreply.github.com>
2021-07-26 20:27:04 +0300

    12 lesson Reactive

---
+++ 012/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h1>Vue Composition Api</h1>
+      <small>data, methods, computed, watch</small>
+      <hr>
+      <p>Название: <strong>{{ name }}</strong></p>
+      <p>Версия: <strong>{{ version }}</strong></p>
+
+      <button class="btn" @click="change">Изменить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive, toRefs} from 'vue'
+
+export default {
+  setup() {
+    // const name = ref('VueJS')
+    // const version = ref('3')
+
+    const framework = reactive({
+      name: 'VueJS',
+      version: '3'
+    })
+
+    function changeInfo() {
+      // name.value = 'Vue JS !'
+      // version.value = 4
+
+      framework.name = 'Vue JS !'
+      framework.version = 4
+    }
+
+    return {
+      // name: framework.value.name,
+      // version: framework.value.version,
+      ...toRefs(framework),
+      framework: framework,
+      change: changeInfo
+    }
+  }
+  // data() {
+  //   return {
+  //     name: 'VueJS',
+  //     version: 3
+  //   }
+  // },
+  // methods: {
+  //   changeInfo() {
+  //     this.name = 'Vue JS!'
+  //     this.version = 4
+  //   }
+  // }
+}
+</script>
